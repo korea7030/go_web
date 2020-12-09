b8146d79450f90bebd58014d6ab536b1447c5566
korea7030 <korea7030@naver.com>
2020-12-09 11:36:03 +0900

    go test

---
+++ myapp/app.go
@@ -0,0 +1,67 @@
+package myapp
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World") // writer에 해당하는 문자열을 출력
+}
+
+type fooHandler struct{}
+
+type User struct {
+	// annotation 추가
+	FirstName string    `mytag:"first_name"`
+	LastName  string    `mytag:"last_name"`
+	Email     string    `mytag:"email"`
+	createdAt time.Time `mytag:"created_at"`
+}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request", err)
+		return
+	}
+	user.createdAt = time.Now()
+	fmt.Print("time : ", user)
+
+	// go structure -> json 형태로 변환(bytearray, err)
+	data, _ := json.Marshal(user)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// string 형태로 변환
+	fmt.Fprint(w, string(data))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name") // url argument
+
+	if name == "" {
+		name = "world"
+	}
+	fmt.Fprintf(w, "Hello %s!", name)
+}
+
+func NewHttpHandler() http.Handler {
+	mux := http.NewServeMux() // route instance 생성
+
+	// 경로에 해당하는 handlerFunc 설정
+	mux.HandleFunc("/", indexHandler)
+
+	mux.HandleFunc("/bar", barHandler)
+
+	// interface 호출
+	mux.Handle("/foo", &fooHandler{})
+
+	// Server Listening
+	http.ListenAndServe(":4000", mux)
+	return mux
+}
---
+++ myapp/app_test.go
@@ -0,0 +1,87 @@
+package myapp
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// test package
+func TestIndexPathHandler(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+	indexHandler(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	// if res.Code != http.StatusBadRequest {
+	// 	t.Fatal("Failed ", res.Code)
+	// }
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data))
+
+}
+
+func TestBarPathHandler_WithoutName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello world!", string(data))
+}
+
+func TestBarPathHandler_WithName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?name=aaa", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello aaa!", string(data))
+}
+
+func TestFooHandler_WithoutJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/foo", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+}
+
+func TestFooHandler_WithJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/foo",
+		strings.NewReader(`{"first_name":"aaa", "last_name":"bbb", "email":"ccc"}`))
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	user := new(User)
+	err := json.NewDecoder(res.Body).Decode(user)
+	assert.Nil(err)
+	assert.Equal("aaa", user.FirstName)
+	assert.Equal("bbb", user.LastName)
+}
