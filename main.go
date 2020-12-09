da20f66aa90453a3e858c2fc0b589ffdea1f5d45
korea7030 <korea7030@naver.com>
2020-12-09 19:36:15 +0900

    Web DecoHandler

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
+
+	"web1/decoHandler"
 	"web1/myapp"
 )
 
+func logger(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Println("[LOGGER1] Started")
+	h.ServeHTTP(w, r)
+	log.Println("[LOGGER1] Completed time:", time.Since(start).Milliseconds())
+}
+
+func logger2(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Println("[LOGGER2] Started")
+	h.ServeHTTP(w, r) // logger1 호출
+	log.Println("[LOGGER2] Completed time:", time.Since(start).Milliseconds())
+}
+
+func NewHandler() http.Handler {
+	h := myapp.NewHandler()
+	h = decoHandler.NewDecoHandler(h, logger)
+	h = decoHandler.NewDecoHandler(h, logger2)
+	return h
+}
+
 func main() {
-	http.ListenAndServe(":4000", myapp.NewHandler())
+	mux := NewHandler()
+
+	http.ListenAndServe(":4000", mux)
 }
--- myapp/app.go
+++ myapp/app.go
@@ -1,158 +1,16 @@
 package myapp
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
-	"time"
-
-	"github.com/gorilla/mux"
 )
-
-var userMap map[int]*User
-var lastID int
-
-type User struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-func GetUserInfoHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	user, ok := userMap[id]
-
-	if !ok {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "No User ID:", id)
-		return
-	}
-
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	data, _ := json.Marshal(user)
-	fmt.Fprint(w, string(data))
-}
-
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	if len(userMap) == 0 {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "No Users")
-		return
-	}
-	users := []*User{}
-	for _, u := range userMap {
-		users = append(users, u)
-	}
-	data, _ := json.Marshal(users)
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, string(data))
-}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
 }
 
-func createUserHandler(w http.ResponseWriter, r *http.Request) {
-	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user)
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	// created User
-	lastID++
-	user.ID = lastID
-	user.CreatedAt = time.Now()
-	userMap[user.ID] = user
-
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	data, _ := json.Marshal(user)
-	fmt.Fprint(w, string(data))
-}
-
-func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	_, ok := userMap[id]
-	if !ok {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "No User ID:", id)
-		return
-	}
-
-	delete(userMap, id)
-	fmt.Fprint(w, "Deleted User ID:", id)
-}
-
-func updateUserHandler(w http.ResponseWriter, r *http.Request) {
-	updateUser := new(User)
-	err := json.NewDecoder(r.Body).Decode(updateUser)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	// userMap의 pointer 타입이므로, 업데이트 되면 map도 같이 업데이트
-	user, ok := userMap[updateUser.ID]
-	if !ok {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "No User ID:", updateUser.ID)
-		return
-	}
-
-	if updateUser.FirstName != "" {
-		user.FirstName = updateUser.FirstName
-	}
-	if updateUser.LastName != "" {
-		user.LastName = updateUser.LastName
-	}
-	if updateUser.Email != "" {
-		user.Email = updateUser.Email
-	}
-
-	// userMap[updateUser.ID] = updateUser
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	data, _ := json.Marshal(updateUser)
-	fmt.Fprint(w, string(data))
-}
-
-// NewHandler
 func NewHandler() http.Handler {
-	userMap = make(map[int]*User)
-	lastID = 0
-	mux := mux.NewRouter()
-	// mux := http.NewServeMux()
-
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler).Methods("GET")
-	mux.HandleFunc("/users", createUserHandler).Methods("POST")
-	mux.HandleFunc("/users", updateUserHandler).Methods("PUT")
-	mux.HandleFunc("/users/{id:[0-9]+}", GetUserInfoHandler).Methods("GET")
-	mux.HandleFunc("/users/{id:[0-9]+}", DeleteUserHandler).Methods("DELETE")
 	return mux
 }
