154f7b846d6c71bf291a8348cc3404cdd4f9e935
korea7030 <korea7030@naver.com>
2020-12-10 14:34:09 +0900

    Todo 웹 1

--- main.go
+++ main.go
@@ -1,95 +1,22 @@
 package main
 
 import (
-	"context"
-	"crypto/rand"
-	"encoding/base64"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"time"
+	"web1/myapp"
 
-	"github.com/gorilla/pat"
 	"github.com/urfave/negroni"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 )
 
-// google oauth config 설정
-var googleOauthConfig = oauth2.Config{
-	RedirectURL:  "http://localhost:4000/auth/google/callback",
-	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-	ClientSecret: os.Getenv("GOOGLE_SECRET_KEY"),
-	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-	Endpoint:     google.Endpoint,
-}
-
-// 생성된 csrf token을 통해 config의 AuthCodeURL로 이동(google 계정으로 로그인 페이지)
-func googleLoginHandler(w http.ResponseWriter, r *http.Request) {
-	state := generateStateOauthCookie(w)
-	url := googleOauthConfig.AuthCodeURL(state)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-}
-
-// cookie에 csrf token 생성
-func generateStateOauthCookie(w http.ResponseWriter) string {
-	expiration := time.Now().Add(1 * 24 * time.Hour)
-	b := make([]byte, 16)
-	rand.Read(b)
-	state := base64.URLEncoding.EncodeToString(b)
-	cookie := http.Cookie{
-		Name:    "oauthstate",
-		Value:   state,
-		Expires: expiration,
-	}
-	http.SetCookie(w, &cookie)
-	return state
-}
-
-// google callback url로 설정한 url이 호출한 후 함수
-func googleAuthCallback(w http.ResponseWriter, r *http.Request) {
-	oauthstate, _ := r.Cookie("oauthstate")
-
-	if r.FormValue("state") != oauthstate.Value {
-		log.Printf("invalid google oauth state cookie: %s, state:%s \n", oauthstate.Value, r.FormValue("state"))
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-	}
-	// google에 등록된 user정보 get
-	data, err := getGoogleUserInfo(r.FormValue("code"))
-	if err != nil {
-		log.Println(err.Error())
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
-	fmt.Fprint(w, string(data))
-}
-
-const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
-
-// google에서 갔다온 token정보로 user정보 get
-func getGoogleUserInfo(code string) ([]byte, error) {
-	token, err := googleOauthConfig.Exchange(context.Background(), code)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to Exchange %s\n", err.Error())
-	}
-
-	resp, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
-
-	if err != nil {
-		return nil, fmt.Errorf("Failed to Get user Info %s\n", err.Error())
-	}
-	return ioutil.ReadAll(resp.Body)
-}
-
 func main() {
-	mux := pat.New()
-	mux.HandleFunc("/auth/google/login", googleLoginHandler)
-	mux.HandleFunc("/auth/google/callback", googleAuthCallback)
+	m := myapp.MakeHandler()
 	n := negroni.Classic()
-	n.UseHandler(mux)
+	n.UseHandler(m)
 
-	http.ListenAndServe(":4000", n)
+	log.Println("Started App")
+	err := http.ListenAndServe(":4000", n)
+	if err != nil {
+		panic(err)
+	}
+
 }
